007ab602c3281d554f23a0d7a504980a08281255
Emmanuel Ndubuisi <10370951+ndubuisx@users.noreply.github.com>
2022-01-25 20:27:53 -0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,268 @@
+import bittensor
+import os
+import time
+import datetime
+import bittensor
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import random
+
+import matplotlib.pyplot as plt
+
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split, RandomSampler, SequentialSampler
+torch.manual_seed(42)
+
+from transformers import GPT2LMHeadModel,  GPT2Tokenizer, GPT2Config, GPT2LMHeadModel
+from transformers import AdamW, get_linear_schedule_with_warmup, AutoTokenizer, AutoModelForCausalLM
+from accelerate import Accelerator
+
+import nltk
+nltk.download('punkt')
+
+dataset = bittensor.dataset(no_tokenizer=True)
+batch_size = 2
+mountain = []
+corpus_size = 10000
+checkpoint = 'distilgpt2'
+# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+tokenizer = GPT2Tokenizer.from_pretrained(checkpoint)
+tokenizer.pad_token = tokenizer.eos_token
+# model = AutoModelForCausalLM.from_pretrained(checkpoint)
+model = GPT2LMHeadModel.from_pretrained(checkpoint)
+accelerator = Accelerator()
+
+class GPT2Dataset(Dataset):
+
+  def __init__(self, txt_list, tokenizer, max_length=768):
+
+    self.tokenizer = tokenizer
+    self.input_ids = []
+    self.attn_masks = []
+
+    for txt in txt_list:
+
+      encodings_dict = tokenizer('<|startoftext|>'+ txt + '<|endoftext|>', truncation=True, max_length=max_length, padding="max_length")
+
+      self.input_ids.append(torch.tensor(encodings_dict['input_ids']))
+      self.attn_masks.append(torch.tensor(encodings_dict['attention_mask']))
+    
+  def __len__(self):
+    return len(self.input_ids)
+
+  def __getitem__(self, idx):
+    return self.input_ids[idx], self.attn_masks[idx] 
+
+for batch in dataset.dataloader(corpus_size):
+    mountain = mountain + batch
+
+print(mountain[:5])
+    
+dataset = GPT2Dataset(mountain, tokenizer, max_length=768)
+
+train_size = int(0.9 * len(dataset))
+val_size = len(dataset) - train_size
+
+train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+print('{:>5,} training samples'.format(train_size))
+print('{:>5,} validation samples'.format(val_size))
+print('{:>5,} mountain corpus size'.format(len(mountain)))
+
+train_dataloader = DataLoader(
+            train_dataset,
+            sampler = RandomSampler(train_dataset),
+            batch_size = batch_size 
+        )
+
+validation_dataloader = DataLoader(
+            val_dataset,
+            sampler = SequentialSampler(val_dataset),
+            batch_size = batch_size
+        )
+
+model.resize_token_embeddings(len(tokenizer))
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# model.cuda()
+
+seed_val = 42
+
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
+epochs = 5
+learning_rate = 5e-4
+warmup_steps = 1e2
+epsilon = 1e-8
+
+sample_interval = 100
+
+optimizer = AdamW(model.parameters(),
+                  lr = learning_rate,
+                  eps = epsilon
+                )
+
+train_dataloader, eval_dataloader, model, optimizer = accelerator.prepare(
+    train_dataloader, validation_dataloader, model, optimizer
+)
+
+total_steps = len(train_dataloader) * epochs
+
+scheduler = get_linear_schedule_with_warmup(optimizer, 
+                                            num_warmup_steps = warmup_steps, 
+                                            num_training_steps = total_steps)
+
+def format_time(elapsed):
+    return str(datetime.timedelta(seconds=int(round((elapsed)))))
+
+total_t0 = time.time()
+
+training_stats = []
+
+# model = model.to(device)
+
+print('Model Device: {}'.format("GPU" if torch.cuda.is_available() else "CPU"))
+
+for epoch_i in range(0, epochs):
+    # ========================================
+    #               Training
+    # ========================================
+
+    print("")
+    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
+    print('Training...')
+
+    t0 = time.time()
+
+    total_train_loss = 0
+
+    model.train()
+
+    for step, batch in enumerate(train_dataloader):
+
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[0].to(device)
+        b_masks = batch[1].to(device)
+
+        model.zero_grad()        
+
+        outputs = model(  b_input_ids,
+                          labels=b_labels, 
+                          attention_mask = b_masks,
+                          token_type_ids=None
+                        )
+
+        loss = outputs[0]  
+
+        batch_loss = loss.item()
+        total_train_loss += batch_loss
+
+        if step % sample_interval == 0 and not step == 0:
+
+            elapsed = format_time(time.time() - t0)
+            print('  Batch {:>5,}  of  {:>5,}. Loss: {:>5,}.   Elapsed: {:}.'.format(step, len(train_dataloader), batch_loss, elapsed))
+
+            model.eval()
+
+            sample_outputs = model.generate(
+                                    bos_token_id=random.randint(1,30000),
+                                    do_sample=True,   
+                                    top_k=50, 
+                                    max_length = 200,
+                                    top_p=0.95, 
+                                    num_return_sequences=1
+                                )
+            for i, sample_output in enumerate(sample_outputs):
+                  print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
+            
+            model.train()
+
+        # loss.backward()
+        
+        accelerator.backward(loss)
+
+        optimizer.step()
+
+        scheduler.step()
+
+    avg_train_loss = total_train_loss / len(train_dataloader)       
+    
+    training_time = format_time(time.time() - t0)
+
+    print("")
+    print("  Average training loss: {0:.2f}".format(avg_train_loss))
+    print("  Training epoch took: {:}".format(training_time))
+        
+    # ========================================
+    #               Validation
+    # ========================================
+
+    print("")
+    print("Running Validation...")
+
+    t0 = time.time()
+
+    model.eval()
+
+    total_eval_loss = 0
+    nb_eval_steps = 0
+
+    for batch in validation_dataloader:
+        
+        b_input_ids = batch[0].to(device)
+        b_labels = batch[0].to(device)
+        b_masks = batch[1].to(device)
+        
+        with torch.no_grad():        
+
+            outputs  = model(b_input_ids, 
+#                            token_type_ids=None, 
+                            attention_mask = b_masks,
+                            labels=b_labels)
+          
+            loss = outputs[0]  
+            
+        batch_loss = loss.item()
+        total_eval_loss += batch_loss        
+
+    avg_val_loss = total_eval_loss / len(validation_dataloader)
+    
+    validation_time = format_time(time.time() - t0)    
+
+    print("  Validation Loss: {0:.2f}".format(avg_val_loss))
+    print("  Validation took: {:}".format(validation_time))
+
+    training_stats.append(
+        {
+            'epoch': epoch_i + 1,
+            'Training Loss': avg_train_loss,
+            'Valid. Loss': avg_val_loss,
+            'Training Time': training_time,
+            'Validation Time': validation_time
+        }
+    )
+
+print("")
+print("Training complete!")
+print("Total training took {:} (h:mm:ss)".format(format_time(time.time()-total_t0)))
+
+try:
+    model.push_to_hub("fine-tuned-{}".format(checkpoint))
+except:
+    print("Unable to push model to Huggingface.")
+
+output_dir = './model_dir/'
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+print("Saving model to %s" % output_dir)
+
+model_to_save = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+model_to_save.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
